6ba25944cae5ac92ee488051d437cbcc7980614e
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-03-20 02:27:03 +0600

    bank page has been added

---
+++ login.js
@@ -0,0 +1,39 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+    console.log("submit button has been clicked");
+
+    const email = document.getElementById('email');
+
+    console.log(email.value);
+
+    const password = document.getElementById('password');
+
+    console.log(password.value);
+
+    if(email.value === 'sunjidasrijoni@gmail.com' && password.value === 'secret'){
+        console.log("Valid User");
+    }
+    else{
+        console.log("Invalid User");
+    }
+
+
+    // else{
+    //     console.log("invalid user");
+    // }
+
+    
+})
+
+
+
+
+// const password = document.getElementById('password').addEventListener('keyup', function(e){
+//     console.log(e.target.value);
+// });
+// console.log(password.value);
+
+const password = document.getElementById('password');
+
+const pass = password.value;
+console.log(pass);
+
