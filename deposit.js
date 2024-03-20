a03664873026e68e657bbff89d6f0a2d676ecb84
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-03-21 01:37:10 +0600

    withdraw section has been completed

--- deposit.js
+++ deposit.js
@@ -46,3 +46,49 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
 
     
 });
+
+
+// ------withdraw section----
+
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    console.log("withdraw button clicked");
+
+    let withdrawAmount = document.getElementById('withdraw-amount');
+
+    console.log(withdrawAmount.value);
+
+    let initialWithdrawAmount = document.getElementById('initial-withdraw');
+    console.log(initialWithdrawAmount.innerText);
+
+    let balance = document.getElementById('initial-balance');
+    console.log("balance is" + balance.innerText);
+
+    if(withdrawAmount.value === ''){
+        alert("Please insert a correct amount");
+    }
+    else{
+         let initialWithdrawAmountInt = parseInt(initialWithdrawAmount.innerText); 
+        let newWithdrawAmount = parseInt(withdrawAmount.value);
+         let mainBalance = parseInt(balance.innerText);
+
+         balance.innerText = mainBalance - newWithdrawAmount;
+
+    
+
+     if(initialWithdrawAmountInt > 0 ){
+        initialWithdrawAmount.innerText = initialWithdrawAmountInt + newWithdrawAmount;
+
+     }
+     else{
+
+
+
+          initialWithdrawAmount.innerText = initialWithdrawAmountInt + newWithdrawAmount;
+     }
+
+     }
+
+   withdrawAmount.value = '';
+
+    
+});
